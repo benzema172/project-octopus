f01974c071b00a67d72b50befb4b4d4a13d9e270
benzema172 <purczynskiwiktor@gmail.com>
2026-08-12 11:43:31 +0200

    Load investments module refinement styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import "./octopus-app.css";
 import "./company-selector-refinement.css";
+import "./investments-refinement.css";
 
 export const metadata: Metadata = {
   title: "Project Octopus",
