8818b099693b99396b71675997e83499afdde7e9
benzema172 <purczynskiwiktor@gmail.com>
2026-08-26 09:22:15 +0200

    Align application release badge with 1.2.1

--- lib/app-release.ts
+++ lib/app-release.ts
@@ -1,6 +1,6 @@
 export const APP_RELEASE = {
-  version: "1.2.0",
-  introducedAt: "24.08.2026"
+  version: "1.2.1",
+  introducedAt: "26.08.2026"
 } as const;
 
 export const APP_RELEASE_LABEL = `Project Octopus v${APP_RELEASE.version} • wdrożono ${APP_RELEASE.introducedAt}`;
