e08d4c4d686cc30490ce64d75541ec52a968cd7c
benzema172 <purczynskiwiktor@gmail.com>
2026-09-03 19:24:11 +0200

    Usuń zbędną migrację hardening po korekcie Fleet Core

--- supabase/migrations/20260903132100_fleet_core_300_security_hardening.sql
+++
@@ -1,9 +0,0 @@
-begin;
-
-revoke all on function public.undo_fleet_ai_decision_300(uuid,uuid,uuid) from public,anon,authenticated;
-grant execute on function public.undo_fleet_ai_decision_300(uuid,uuid,uuid) to service_role;
-drop function if exists public.undo_fleet_ai_decision_300(uuid,uuid,uuid,uuid);
-
-insert into public.schema_versions(version) values('20260903_fleet_core_300_security_hardening') on conflict(version) do nothing;
-
-commit;
